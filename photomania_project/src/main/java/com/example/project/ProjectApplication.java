37bcf3d4e2c3e12857d97df8936dc3a148843601
Kimminu7 <rock9024@naver.com>
2025-01-21 16:41:55 +0900

    Update ProjectApplication.java

--- photomania_project/src/main/java/com/example/project/ProjectApplication.java
+++ photomania_project/src/main/java/com/example/project/ProjectApplication.java
@@ -7,8 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication(scanBasePackages = "com.example.project")
-public class ProjectApplication {
-
+public class ProjectApplication { 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectApplication.class, args);
 	}
