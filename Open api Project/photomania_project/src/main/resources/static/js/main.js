9b92cdfdba90609712c7e22c39aa8097545e1229
Kimminu7 <rock9024@naver.com>
2025-02-03 10:08:50 +0900

    Swiper ( 비로그인상태 비작동 오류 해결 )

--- Open api Project/photomania_project/src/main/resources/static/js/main.js
+++ Open api Project/photomania_project/src/main/resources/static/js/main.js
@@ -1,28 +1,18 @@
 // Swiper 초기화
-document.addEventListener("DOMContentLoaded", function() {
-  var swiper = new Swiper('.swiper-container', {
-    loop: true,
-    slidesPerView: 1,
-    spaceBetween: 10,
-    autoplay: {
-      delay: 3000,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    on: {
-      init: function () {
-        this.update();
-      },
-      resize: function () {
-        this.update();
-      }
-    }
-  });
+document.addEventListener("DOMContentLoaded", function () {
+    var swiper = new Swiper(".swiper-container", {
+        loop: true,
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
 });
